2142f218f5ec3b9b4a58e61f51e9a33de1daf6d0
katehjd21 <katedrew2103@hotmail.co.uk>
2025-09-30 10:45:33 +0100

    completing exercise 5

--- exercises/5-first-iac-pipeline/simple_iac/provider.tf
+++ exercises/5-first-iac-pipeline/simple_iac/provider.tf
@@ -23,11 +23,11 @@ provider "aws" {
 # TODO replace bucket and kms_key_id below with your own values
 terraform {
   backend "s3" {
-    bucket         = "your-own-persistent-state-bucket-name"
+    bucket         = "kates-s3-bucket"
     key            = "pl2025_simple/terraform.tfstate"
     region         = "eu-west-2"
     encrypt        = true
-    kms_key_id     = "alias/your-own-kms-key-alias-name"
+    kms_key_id     = "kates-s3-bucket"
     use_lockfile = true
   }
 }
