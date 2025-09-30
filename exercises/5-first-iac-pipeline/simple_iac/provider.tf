431d7e83f8fe91fefa01be724f1996c11018d133
katehjd21 <katedrew2103@hotmail.co.uk>
2025-09-30 14:02:22 +0100

    changing provider.tf

--- exercises/5-first-iac-pipeline/simple_iac/provider.tf
+++ exercises/5-first-iac-pipeline/simple_iac/provider.tf
@@ -27,7 +27,7 @@ terraform {
     key            = "pl2025_simple/terraform.tfstate"
     region         = "eu-west-2"
     encrypt        = true
-    kms_key_id     = "alias/kates-s3-bucket"
+    kms_key_id     = "alias/kates_kms_alias"
     use_lockfile = true
   }
 }
